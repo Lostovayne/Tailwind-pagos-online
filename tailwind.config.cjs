c0e9b0b3377baca34876a31bc6871a1305df63f3
Losto <92962731+Lostovayne@users.noreply.github.com>
2023-01-12 18:10:56 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html','./src/pages/pago-parte2.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+// Hola soy tailwind callense 
